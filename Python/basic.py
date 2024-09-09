0006e610442d37062ad47b4444a12f4f3c999eda
baburam1997 <shrestha.baburam1997@gmail.com>
2024-09-09 22:49:37 +0545

    basic python file added

---
+++ Python/basic.py
@@ -0,0 +1,63 @@
+# # # print("Hello, I am python programmer.")
+# # import sys
+# import numpy
+
+# # name = "Baburam Shrestha"
+# # # name is variable  holding the value "Baburam Shrestha"
+# # age = 52
+# # ages = 52.45
+# # # printname, age ,sep="---")
+# # s = ''
+# # # print(type(s))
+# # # print(type(age))
+# # # print(type(ages))
+# # # print(type(name))
+
+# # complex_number = 5+6j
+# # print(type(complex_number))
+
+# _age = 63
+# # 3_variable = 88
+# # %_na = 33
+
+# Age = 89
+# age = 890
+
+# myAge = 54 # pascal casing
+# my_age = 543 # snake casing
+# MyAge = 98 # camel casing
+
+# def fun(a,b):
+#     sum=a+b
+#     print(sum) # sum is local variable to fun function
+#     print(my_age) # my_age is global variable
+
+# print(sum) # generates error
+
+
+# def add(a,b):
+#     sum=a+b
+#     # return sum
+
+# addition = add(2,8)
+# print(addition)
+# Arithmetic Operators
+n1 = 5
+n2 = 4
+# print(n1+n2) # 9
+# print(n1-n2) # 1
+# print(n1*n2) # 20
+# print(n1/n2) # 1.25 actual divison
+# print(n1//n2) # 1 quotient
+# print(n1%n2) # 1 reminder
+# print(n1**n2) # 625 exponent (5^4)
+# Comparision operators
+print(n1==n2)
+print(n1!=n2)
+print(n1<n2)
+print(n1<=n2)
+print(n1>n2)
+print(n1>=n2)
+# Comparison operators generates boolean values (True, False)
+
+
